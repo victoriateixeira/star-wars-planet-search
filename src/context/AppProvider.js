8bffd645142b27723d9390148dc8f5c775762c83
Victoria Teixeira <victoriafteixeira@hotmail.com>
2022-11-23 14:30:26 +1000

    req 1: finalizado

---
+++ src/context/AppProvider.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import AppContext from './AppContext';
+
+function AppProvider({ children }) {
+  const [planets, setPlanets] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchPlanets = async () => {
+    try {
+      const response = await fetch('https://swapi.dev/api/planets');
+      const planetsData = await response.json();
+      console.log(planetsData);
+      const planetsList = planetsData.results;
+      console.log(planetsList);
+      planetsList.forEach((planet) => delete (planet.residents));
+      console.log(planetsList);
+      setPlanets(planetsList);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const values = {
+    isLoading,
+    fetchPlanets,
+    planets,
+  };
+
+  return (
+    <AppContext.Provider value={ values }>
+      {children}
+    </AppContext.Provider>
+  );
+}
+AppProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppProvider;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import AppProvider from './context/AppProvider';
 
-ReactDOM
-  .createRoot(document.getElementById('root'))
-  .render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </BrowserRouter>,
+
+);
